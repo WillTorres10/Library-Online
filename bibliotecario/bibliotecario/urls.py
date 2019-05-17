8ca686cb6b7f0e93ce9b1170d59044aa700a3cb7
William Torres <willian.torres131@gmail.com>
2019-05-17 15:34:14 -0300

    URLs

--- bibliotecario/bibliotecario/urls.py
+++ bibliotecario/bibliotecario/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls', namespace='api')),
+    path('bibliotecario/', include('api.urls', namespace='api')),
 ]
--- livros/livros/urls.py
+++ livros/livros/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('api.urls')),
+    path('livro/api/', include('api.urls')),
 ]
